c5399c4d8980c5985713c5c6d1ee08fe4607b1ac
vnutkin <vl_utkin@yahoo.com>
2024-12-02 11:07:15 +0300

    last variant
    save and restore

--- main.py
+++ main.py
@@ -1,129 +1,170 @@
 class Animal:
-    def __init__(self, name, age):
-        self.name = name
-        self.age =  age
+    def __init__(self, args):
+        self.name = args[0]
+        self.age = args[1]
+
     def make_sound(self):
-        print('издает звук')
+        print(f'{self.name} издает звук')
+
     def eat(self):
-        print('ест')
+        print(f'{self.name} ест')
+
+
 class Bird(Animal):
-    def __init__(self, name, age, sound, food):
-        super().__init__(name, age)
-        self.sound = sound
-        self.food = food
+    def __init__(self, args):
+        super().__init__(args)
+        self.sound = args[2]
+        self.food = args[3]
+
     def make_sound(self):
-        super().make_sound(self)
+        super().make_sound()
         print(self.sound)
+
     def eat(self):
-        super().eat(self)
+        super().eat()
         print(self.food)
+
     def save(self):
-        return 'Bird'+'|'+self.name+'|'+self.age+'|'+self.sound+'|'+self.food
+        return 'Bird' + '|\''+self.name+'\'|' + str(self.age) + '|\''+self.sound+'\'|\''+self.food+'\''
+
+
 class Mammal(Animal):
-    def __init__(self, name, age, sound, food):
-        super().__init__(name, age)
-        self.sound = sound
-        self.food = food
+    def __init__(self, args):
+        super().__init__(args)
+        self.sound = args[2]
+        self.food = args[3]
+
     def make_sound(self):
-        super().make_sound(self)
+        super().make_sound()
         print(self.sound)
+
     def eat(self):
-        super().eat(self)
+        super().eat()
         print(self.food)
+
     def save(self):
-        return 'Mammal'+'|'+self.name+'|'+self.age+'|'+self.sound+'|'+self.food
+        return 'Mammal' + '|\''+self.name+'\'|' + str(self.age) + '|\''+self.sound+'\'|\''+self.food+'\''
+
 class Reptile(Animal):
-    def __init__(self, name, age, sound, food):
-        super().__init__(name, age)
-        self.sound = sound
-        self.food = food
+    def __init__(self, args):
+        super().__init__(args)
+        self.sound = args[2]
+        self.food = args[3]
+
     def make_sound(self):
-        super().make_sound(self)
+        super().make_sound()
         print(self.sound)
+
     def eat(self):
-        super().eat(self)
+        super().eat()
         print(self.food)
+
     def save(self):
-        return 'Reptile'+'|'+self.name+'|'+self.age+'|'+self.sound+'|'+self.food
+        return 'Reptile' + '|\''+self.name+'\'|' + str(self.age) + '|\''+self.sound+'\'|\''+self.food+'\''
+
+
 class Person:
-    def __init__(self, name, birth_date):
-        self.name = name
-        self.birth_date = birth_date
+    def __init__(self, args):
+        self.name = args[0]
+        self.birth_date = args[1]
+
+
 class Veterinarian(Person):
     def heal_animal(self, animal):
         print(f'лечит {animal.name}')
+
     def save(self):
-        return 'Veterinarian'+'|'+self.name+'|'+self.birth_date
+        return 'Veterinarian'+'|\''+self.name+'\'|\''+self.birth_date+'\''
+
+
 class ZooKeeper(Person):
     def feed_animal(self, animal):
         print(f'кормит {animal.name}, дает {animal.food} ')
+
     def save(self):
-        return 'ZooKeeper'+'|'+self.name+'|'+self.birth_date
+        return 'ZooKeeper'+'|\''+self.name+'\'|\''+self.birth_date+'\''
+
+
 class Zoo:
     def __init__(self):
         self.__animals = []
         self.__persons = []
+
     def animal_sound(self):
         for animal in self.__animals:
             animal.make_sound()
+
+    def animal_eat(self):
+        for animal in self.__animals:
+            animal.eat()
+
     def add_animal(self, animal):
         self.__animals.append(animal)
+
     def add_person(self, person):
         self.__persons.append(person)
+
     def list_animals(self):
         for animal in self.__animals:
             print(f'Животное {animal.name}, возраст {animal.age}')
+
     def list_persons(self):
         for person in self.__persons:
             print(f'Сотрудник {person.name}, дата рождения {person.birth_date}')
-    def save_history(self,history):
-        file = open(history,'w')
-        file.write(str(len(self.__animals))+'\n')
-        for animal in self.__animals:
-            file.write(animal.save()+'\n')
-        file.write(str(len(self.__persons))+'\n')
-        for person in self.__persons:
-            file.write(person.save()+'\n')
-   def restore_history(self,history):
-        file = open(history,'r')
-        file.read(line)
-        len_list = int(line)
-        self.__animals = []
-        for i in range(len_list):
-            file.read(line)
-            list_in = line.split(sep='|')
-            animal = globals()[list_in(0)].restore(list_in[1:])
-            self.__animals.append(animal)
-        file.read(line)
-        len_list = int(line)
-        self.__persons = []
-        for i in range(len_list):
-            file.read(line)
-            list_in = line.split(sep='|')
-            person = globals()[list_in(0)].restore(list_in[1:])
-            self.__persons.append(person)
+
+    def save_history(self, history):
+        with open(history, 'w') as file:
+            file.write(str(len(self.__animals)) + '\n')
+            for animal in self.__animals:
+                file.write(animal.save() + '\n')
+            file.write(str(len(self.__persons)) + '\n')
+            for person in self.__persons:
+                file.write(person.save() + '\n')
+
+    def restore_history(self, history):
+        with open(history, 'r') as file:
+            line = file.readline()
+            len_list = int(line)
+            self.__animals = []
+            for i in range(len_list):
+                line = file.readline()
+                list_in = line.split(sep='|')
+                animal = globals()[list_in[0]](list_in[1:])
+                self.__animals.append(animal)
+            line = file.readline()
+            len_list = int(line)
+            self.__persons = []
+            for i in range(len_list):
+                line = file.readline()
+                list_in = line.split(sep='|')
+                person = globals()[list_in[0]](list_in[1:])
+                self.__persons.append(person)
+
 
 bird = Bird
-b1 = bird(name='Птица воробей', age='2', sound='чирик',food='зерно')
-b2 = bird(name='Птица журавль', age='4', sound='курлык',food='лягушки')
+b1 = bird(['Птица воробей', 2, 'чирик', 'зерно'])
+b2 = bird(['Птица журавль', 4, 'курлык', 'лягушки'])
 mammal = Mammal
-m1 = mammal(name='Млекопитающее коза', age='5', sound='бее',food='трава')
-m2 = mammal(name='Млекопитающее лошадь', age='6', sound='игого',food='сено')
+m1 = mammal(['Млекопитающее коза', 5, 'бее', 'трава'])
+m2 = mammal(['Млекопитающее лошадь', 6, 'игого', 'сено'])
 zoo = Zoo()
 zoo.add_animal(b1)
 zoo.add_animal(b2)
 zoo.add_animal(m1)
 zoo.add_animal(m2)
 veter = Veterinarian
-v1 = veter('Ветеринар Иванов','21.12.1975')
+v1 = veter(['Ветеринар Иванов', '21.12.1975'])
 zoo.add_person(v1)
 zookip = ZooKeeper
-z1 = zookip('Служитель Петров','15.02.1980')
-z2 = zookip('Служитель Сидоров','15.04.1989')
+z1 = zookip(['Служитель Петров', '15.02.1980'])
+z2 = zookip(['Служитель Сидоров', '15.04.1989'])
 zoo.add_person(z1)
 zoo.add_person(z2)
 zoo.list_animals()
 zoo.list_persons()
+zoo.animal_sound()
+zoo.animal_eat()
 zoo.save_history('history.txt')
-
-
+zoo.restore_history('history.txt')
+zoo.list_animals()
+zoo.list_persons()
